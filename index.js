914d7f2e417bfa6b3f52e7b17a8bf9a1bd790858
Md Siam Maruf <siam.maruf1@gmail.com>
2023-01-26 12:01:19 +0600

    Working on Event Emitter

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Logged = require("./logger");
 const logger = new Logged();
 
 logger.on("messageLogger",(arg)=>{
-    console.log(`Listener Enabled ${arg}`);
+    console.log(`I am working on Event Emitter. My name is ${arg.name}`);
 });
 
 logger.log("Message")
